ec633b5f7dc0919858020f81560024e55a09240f
Devika-sajeev1010 <devikadiya1010@gmail.com>
2025-06-30 15:23:48 +0530

    Added

--- src/program3.java
+++ src/program3.java
@@ -1,2 +1,16 @@
+
+import java.util.Scanner;
+
 public class program3 {
+    public static void main(String[] s)
+    {
+        int year;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter year: ");
+        year = sc.nextInt();
+        if((year%400==0 && year%100==0) ||  (year%4==0 && year%100!=0))
+            System.out.println("Leap year");
+        else
+            System.out.println("not a leap year");
+    }
 }

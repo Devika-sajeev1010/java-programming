9fb0194b121910f075414a3a113edf988619635a
Devika-sajeev1010 <devikadiya1010@gmail.com>
2025-07-08 15:06:04 +0530

    july8

---
+++ src/program6.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+public class program6 {
+    public static void main(String[] s) {
+        int max = 500, min = 600;
+        double a = Math.random() * (max - min + 1) + min;
+        System.out.println(a);
+
+
+    }
+}

2effd6122ade5d26998295302b9d665bad878066
Devika-sajeev1010 <devikadiya1010@gmail.com>
2025-07-28 14:37:38 +0530

    28-07-2025

---
+++ src/program18.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class program18 {
+    int  num , temp;
+    Scanner sc =new Scanner(System.in);
+    public static void main(String[] args)
+    {
+
+    }
+    void input()
+    {
+        System.out.println("Enter the number");
+        num = sc.nextInt();
+        temp = num;
+
+    }
+//    void check(){
+//        num
+//    }
+}
---
+++ src/program20.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class program20 {
+    public static void main(String[] s)
+    {
+        int [][]arr = new int [3][3];
+        Scanner sc = new Scanner(System.in);
+        for(int i =0; i<3;i++)
+        {
+            for(int j = 0; j<3 ; j++)
+            {
+                System.out.println("enter the numbers");
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0; i< 3; i++)
+        {
+            for(int j =0 ;j< 3; j++)
+            {
+                if(arr[i][j] != arr[j][i] && i!= j)
+                {
+                    System.out.println("not symmetric");
+                    exit(0);
+                }
+            }
+        }
+        System.out.println("Matrix is symmetric");
+    }
+}
